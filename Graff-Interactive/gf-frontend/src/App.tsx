3266aeec9170748ba1a0afdfff922aab1053925d
fet1sov <august9222@gmail.com>
2024-04-22 20:25:23 +0500

    GRAFF: Added the router on frontend part
    Frontend:
      - Added the router

--- Graff-Interactive/gf-frontend/src/App.tsx
+++ Graff-Interactive/gf-frontend/src/App.tsx
@@ -1,15 +1,35 @@
 import './assets/styles.css';
 
-import SiteNavigation from './components/SiteNavigation/SiteNavigation';
-import SiteChat from './components/SupportChat/SupportChat';
+// import SiteNavigation from './components/SiteNavigation/SiteNavigation';
+// import SiteChat from './components/SupportChat/SupportChat';
+
+import { BrowserRouter, Routes, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import SupportPage from './pages/SupportPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />
+  },
+  {
+    path: "/support",
+    element: <SupportPage />,
+  }
+  // {
+  //   path: "*",
+  //   element: <NotFound />,
+  // },
+])
 
 function App() {
   return (
     <>
-      <SiteNavigation />
-      <div className="container">
-        <SiteChat />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <RouterProvider router={router} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
